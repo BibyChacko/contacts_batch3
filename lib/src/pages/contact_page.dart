7d3df073e23959d485452d80f5a4dc9d75633420
almubdie <bibychackokpply@gmail.com>
2022-09-22 20:55:44 +0530

    Multiple contact handled

--- lib/src/pages/contact_page.dart
+++ lib/src/pages/contact_page.dart
@@ -1,5 +1,5 @@
-
 import 'package:contact/src/helpers/common_actions.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:contacts_service/contacts_service.dart';
@@ -13,6 +13,7 @@ class ContactPage extends StatefulWidget {
 
 class _ContactPageState extends State<ContactPage> {
   List<Contact> contacts = [];
+  bool isLoading = true;
 
   @override
   void initState() {
@@ -26,45 +27,49 @@ class _ContactPageState extends State<ContactPage> {
       appBar: AppBar(
         title: Text("Contacts"),
       ),
-      body: ListView.builder(
-        itemCount: contacts.length,
-        itemBuilder: (context, index) {
-          Contact contactItem = contacts[index];
-          String mobNos = (contactItem.phones ?? [])
-              .map((e) => e.value ?? "")
-              .toList()
-              .join(" ");
-          return ListTile(
-            title: Text("${contactItem.displayName}"),
-            subtitle: Text(mobNos),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                    onPressed: () {
-                      String mobNo = contactItem.phones?.first.value ?? "";
-                      CommonAction.sendSms(mobNo, "HappyBirthday");
-                    },
-                    icon: Icon(Icons.sms_outlined)),
-                IconButton(
-                    onPressed: () {
-                      // If user have multiple numbers
-                      if(contactItem.phones == null){
-                        return;
-                      }
-                      if(contactItem.phones!.length>1){
-                        // Showing a dialog to opt the number
-                        showContactNumberDialog(context,contactItem.phones!);
-                      }else{
-                        String mobNo = contactItem.phones!.first.value ?? "";
-                        CommonAction.makeCall(mobNo);
-                      }
-                    },
-                    icon: Icon(Icons.call)),
-              ],
-            ),
-          );
-        },
+      body: Visibility(
+        visible: !isLoading,
+        replacement: Center(child: CupertinoActivityIndicator()),
+        child: ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            Contact contactItem = contacts[index];
+            String mobNos = (contactItem.phones ?? [])
+                .map((e) => e.value ?? "")
+                .toList()
+                .join(" ");
+            return ListTile(
+              title: Text("${contactItem.displayName}"),
+              subtitle: Text(mobNos),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        String mobNo = contactItem.phones?.first.value ?? "";
+                        CommonAction.sendSms(mobNo, "HappyBirthday");
+                      },
+                      icon: Icon(Icons.sms_outlined)),
+                  IconButton(
+                      onPressed: () {
+                        // If user have multiple numbers
+                        if (contactItem.phones == null) {
+                          return;
+                        }
+                        if (contactItem.phones!.length > 1) {
+                          // Showing a dialog to opt the number
+                          showContactNumberDialog(context, contactItem.phones!);
+                        } else {
+                          String mobNo = contactItem.phones!.first.value ?? "";
+                          CommonAction.makeCall(mobNo);
+                        }
+                      },
+                      icon: Icon(Icons.call)),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
@@ -77,40 +82,48 @@ class _ContactPageState extends State<ContactPage> {
           await ContactsService.getContacts(); // Asynchronous - Memory
       setState(() {
         contacts = contactsTemp;
+        isLoading = false;
       });
       // Read the contacts
     }
   }
 
   void showContactNumberDialog(BuildContext context, List<Item> phone) {
-   showDialog(context: context,
-       builder: (context){
+    showDialog(
+        context: context,
+        builder: (context) {
           return AlertDialog(
             title: Text("Choose the number"),
-            content: ListView.builder(
-              itemCount: phone.length,
-              itemBuilder: (context,index){
-                Item item = phone[index];
-                return ListTile(
-                  title: Text(item.value ?? ""),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(onPressed: (){
-                        String mobNo = item.value ?? "";
-                        CommonAction.sendSms(mobNo,"");
-                      }, icon: Icon(Icons.sms)),
-                      IconButton(onPressed: (){
-                        String mobNo = item.value ?? "";
-                        CommonAction.makeCall(mobNo);
-                      }, icon: Icon(Icons.call)),
-                    ],
-                  ),
-                );
-              },
+            content: Container(
+              height: 550,
+              child: ListView.builder(
+                itemCount: phone.length,
+                itemBuilder: (context, index) {
+                  Item item = phone[index];
+                  return ListTile(
+                    title: Text(item.value ?? ""),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              String mobNo = item.value ?? "";
+                              CommonAction.sendSms(mobNo, "Happy birthday");
+                            },
+                            icon: const Icon(Icons.sms)),
+                        IconButton(
+                            onPressed: () {
+                              String mobNo = item.value ?? "";
+                              CommonAction.makeCall(mobNo);
+                            },
+                            icon: Icon(Icons.call)),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
           );
-       }
-   );
+        });
   }
 }
